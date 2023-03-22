5f34893dc4be86b04f9591ef594ac0862d2dfe35
Luke Grunau <lukegrunau@gmail.com>
2023-03-22 11:25:49 +0100

    CType Details (#5)

--- src/backend/utilities/supportedCTypes.ts
+++ src/backend/utilities/supportedCTypes.ts
@@ -1,39 +1,23 @@
 import type { ICType } from '@kiltprotocol/sdk-js';
 
-import { discordCType } from '../cTypes/discordCType';
 import { emailCType } from '../cTypes/emailCType';
-import { githubCType } from '../cTypes/githubCType';
-import { telegramCType } from '../cTypes/telegramCType';
-import { twitchCType } from '../cTypes/twitchCType';
-import { twitterCType } from '../cTypes/twitterCType';
-import { youtubeCType } from '../cTypes/youtubeCType';
 
-export const supportedCTypeKeys = [
-  'discord',
-  'email',
-  'github',
-  'telegram',
-  'twitch',
-  'twitter',
-  'youtube',
-] as const;
+import { twitterCType } from '../cTypes/twitterCType';
+
+export const supportedCTypeKeys = ['email', 'twitter'] as const;
 
 export type SupportedCType = (typeof supportedCTypeKeys)[number];
 
 export const supportedCTypes: Record<SupportedCType, ICType> = {
-  discord: discordCType,
   email: emailCType,
-  github: githubCType,
-  telegram: telegramCType,
-  twitch: twitchCType,
   twitter: twitterCType,
-  youtube: youtubeCType,
 };
 
-export const socialCTypeIds = [
-  supportedCTypes.discord.$id,
-  supportedCTypes.telegram.$id,
-  supportedCTypes.twitter.$id,
-  supportedCTypes.twitch.$id,
-  supportedCTypes.youtube.$id,
-];
+export const kiltCost: Record<SupportedCType, string> = {
+  email: '2',
+  twitter: '3',
+};
+
+export function isSupportedCType(cType: string): cType is SupportedCType {
+  return supportedCTypeKeys.includes(cType as SupportedCType);
+}
--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,61 +1,202 @@
 import { createRoot } from 'react-dom/client';
 
-import type { ICType } from '@kiltprotocol/sdk-js';
-
-import { FormEvent, Fragment, useCallback, useState } from 'react';
+import { Fragment, useCallback, useEffect, useState } from 'react';
 import {
-  SupportedCType,
-  supportedCTypeKeys,
+  BrowserRouter,
+  generatePath,
+  Link,
+  Route,
+  Routes,
+  useParams,
+} from 'react-router-dom';
+
+import {
+  kiltCost,
   supportedCTypes,
+  supportedCTypeKeys,
+  isSupportedCType,
 } from '../backend/utilities/supportedCTypes';
+import {
+  apiWindow,
+  getCompatibleExtensions,
+  getSession,
+  Session,
+} from './utilities/session';
+import { exceptionToError } from './utilities/exceptionToError';
 
-function Claim({ cType }: { cType: ICType }) {
-  const { title } = cType;
+import { paths } from './utilities/paths';
 
+function Connect({ setSession }: { setSession: (s: Session) => void }) {
+  const { kilt } = apiWindow;
+
+  const [extensions, setExtensions] = useState(getCompatibleExtensions());
+
+  const [processing, setProcessing] = useState(false);
+  const [error, setError] = useState<'closed' | 'rejected' | 'unknown'>();
+
+  useEffect(() => {
+    function handler() {
+      setExtensions(getCompatibleExtensions());
+    }
+
+    window.dispatchEvent(new CustomEvent('kilt-dapp#initialized'));
+    window.addEventListener('kilt-extension#initialized', handler);
+    return () =>
+      window.removeEventListener('kilt-extension#initialized', handler);
+  }, []);
+
+  const handleConnect = useCallback(
+    async (extension: string) => {
+      try {
+        setProcessing(true);
+        setError(undefined);
+
+        setSession(await getSession(kilt[extension]));
+      } catch (exception) {
+        const { message } = exceptionToError(exception);
+        if (message.includes('closed')) {
+          setError('closed');
+        } else if (message.includes('Not authorized')) {
+          setError('rejected');
+        } else {
+          setError('unknown');
+          console.error(exception);
+        }
+        setProcessing(false);
+      }
+    },
+    [setSession, kilt],
+  );
   return (
     <section>
-      <h2>{title}</h2>
-      {/* TODO: Connect to wallet and claim form */}
+      {extensions.length === 0 && (
+        <p>
+          Looking for a wallet… To make a claim you need to have e.g. Sporran
+          wallet installed and have an identity configured in it.
+        </p>
+      )}
+
+      {extensions.map((extension) => (
+        <button
+          key={extension}
+          type="button"
+          onClick={() => handleConnect(extension)}
+        >
+          Connect to {kilt[extension].name}
+        </button>
+      ))}
+
+      {processing && <p>Connecting…</p>}
+
+      {error === 'unknown' && (
+        <p>
+          Something went wrong! Try again or reload the page or restart your
+          browser.
+        </p>
+      )}
+
+      {error === 'closed' && <p>Your wallet was closed. Please try again.</p>}
+
+      {error === 'rejected' && (
+        <p>
+          The authorization was rejected. Follow the instructions on our Tech
+          Support site to establish the connection between this attester and
+          your wallet.
+        </p>
+      )}
+
+      {error === 'rejected' && (
+        <a
+          href="https://support.kilt.io/support/solutions/articles/80000968082-how-to-grant-access-to-website"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Tech Support
+        </a>
+      )}
     </section>
   );
 }
 
-function App() {
-  const [cType, setCType] = useState<ICType>();
+function Claim() {
+  const { type } = useParams();
 
-  const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const selected = formData.get('cType') as SupportedCType;
-    setCType(supportedCTypes[selected]);
+  const [session, setSession] = useState<Session>();
+
+  const handleSubmit = useCallback(() => {
+    // TODO: handle submit terms
   }, []);
 
+  if (!type || !isSupportedCType(type)) {
+    return <p>Error - Unsupported CType</p>;
+  }
+
+  const cType = supportedCTypes[type];
+  const { title, properties } = cType;
+
   return (
-    <Fragment>
-      <h1>KILT Attester Example</h1>
+    <section>
+      <h2>{title}</h2>
 
-      {!cType && (
+      {!session && (
+        <Fragment>
+          <ul>
+            {Object.keys(properties).map((property) => (
+              <li key={property}>{property}</li>
+            ))}
+          </ul>
+
+          <Connect setSession={setSession} />
+        </Fragment>
+      )}
+
+      {session && (
+        // implement custom claim form if you want to handle non-string properties
         <form onSubmit={handleSubmit}>
-          <label>
-            What type of credential would you like?
-            <select name="cType">
-              <option value="">--Select a cType--</option>
-              {supportedCTypeKeys.map((cTypeKey) => (
-                <option key={cTypeKey} value={cTypeKey}>
-                  {cTypeKey}
-                </option>
-              ))}
-            </select>
-          </label>
+          {Object.keys(properties).map((property) => (
+            <label key={property}>
+              {property} <input name={property} required />
+            </label>
+          ))}
 
-          <button>Continue</button>
+          <p>Price: {kiltCost[type]} KILT</p>
+
+          <button>Submit</button>
         </form>
       )}
 
-      {cType && <Claim cType={cType} />}
-    </Fragment>
+      <Link to={paths.home}>Back</Link>
+    </section>
+  );
+}
+
+function Home() {
+  return (
+    <section>
+      <h1>KILT Attester Example</h1>
+
+      <ul>
+        {supportedCTypeKeys.map((type) => (
+          <li key={type}>
+            <Link to={generatePath(paths.claim, { type })}>
+              {supportedCTypes[type].title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
 
 const root = createRoot(document.querySelector('#app') as HTMLElement);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path={paths.home} element={<Home />} />
+      <Route path={paths.claim} element={<Claim />} />
+      {/* TODO: Admin route */}
+
+      <Route path="*" element={<p>404 - Not found</p>} />
+    </Routes>
+  </BrowserRouter>,
+);
